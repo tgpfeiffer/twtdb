6a3a8166fed2cbf203c55763670921a36448e542
Tobias Pfeiffer <tgpfeiffer@web.de>
2013-12-07 14:39:34 +0900

    added logout button to login form (only one of login or logout is displayed)

--- src/main/scala/net/nablux/twtdb/snippet/LoginForm.scala
+++ src/main/scala/net/nablux/twtdb/snippet/LoginForm.scala
@@ -5,19 +5,28 @@ import net.liftweb.common._
 import net.liftweb.http._
 import net.liftweb.util.Helpers._
 import net.nablux.twtdb.lib._
+import scala.xml.NodeSeq
+import bootstrap.liftweb.Site
 
 class LoginForm extends Loggable {
 
   def oauthButton = {
     UserAccessToken.get match {
       case Full(token) => {
-        // there is already an access token, no need to login
-        ":submit" #> ("already logged in")
+        // we do have an access token, so we render the logout button
+
+        "#loginBtn" #> NodeSeq.Empty &
+          "#logoutBtn" #> SHtml.onSubmitUnit(() => {
+            UserRequestToken.set(Empty)
+            UserAccessToken.set(Empty)
+            S.seeOther(Site.oauthLogin.loc.calcDefaultHref)
+          })
       }
       case _ => {
-        // we don't have an access token yet, so we render the button
-        ":submit" #> SHtml.onSubmitUnit(() =>
-          OauthHelper.getRequestTokenAndRedirect)
+        // we don't have an access token yet, so we render the login button
+        "#loginBtn" #> SHtml.onSubmitUnit(() =>
+          OauthHelper.getRequestTokenAndRedirect) &
+          "#logoutBtn" #> NodeSeq.Empty
       }
     }
   }
